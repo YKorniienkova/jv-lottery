29122783d8a6623eb2a276cfff64ad9fffc4ba70
Yelyzaveta Korniienkova <yelyzavetakorniienkova@gmail.com>
2026-01-19 11:45:21 +0100

    Added Ball class with fields, toString method and constructor

---
+++ src/main/java/core/basesyntax/Ball.java
@@ -0,0 +1,19 @@
+package core.basesyntax;
+
+public class Ball {
+    String color;
+    int number;
+
+    @Override
+    public String toString() {
+        return "Ball{" +
+                "color='" + color + '\'' +
+                ", number=" + number +
+                '}';
+    }
+
+    public Ball(String color, int number) {
+        this.color = color;
+        this.number = number;
+    }
+}
--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,7 +1,14 @@
 package core.basesyntax;
 
+
+import java.util.Random;
+
 public class ColorSupplier {
+    Random random =new Random();
+    static Color[] colors=Color.values();
+    private static final int FIGURE_COUNT = colors.length;
     public String getRandomColor() {
-        return null;
+        int index = random.nextInt(colors.length);
+        return colors[index].toString();
     }
 }
