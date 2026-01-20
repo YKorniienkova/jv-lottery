e9bf79c93314808191d0db59f56a9f062ec243d3
Yelyzaveta Korniienkova <yelyzavetakorniienkova@gmail.com>
2026-01-20 11:19:23 +0100

    fixed problems again

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -4,7 +4,7 @@ import java.util.Random;
 
 public class ColorSupplier {
     private Random random = new Random();
-    private static Color[] colors = Color.values();
+    private Color[] colors = Color.values();
 
     public String getRandomColor() {
         return colors[random.nextInt(colors.length)].name();
